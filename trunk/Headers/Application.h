c717faac323eb716ea53bde219b5ae41a4f41855
Rainer Sieger <rsieger@pangaea.de>
2015-12-14 08:27:02 +0100

    show Progress Bar?

--- trunk/Headers/Application.h
+++ trunk/Headers/Application.h
@@ -89,6 +89,8 @@ public:
     int         gi_EOL;                             //!< End-of-Line Zeichen fr die Outputdateien
     int         gi_Extension;                       //! Dateierweiterung fr Ausgabedateien
 
+    bool        gb_showProgressBar;                 //!< Soll der ProgressBar im Fenster unten rechts angezeigt werden?
+
     // find area
     QString     gs_AreaDatabaseFilename;             //!< Name der Area database
     bool        gb_fa_DeleteInputFile;               //!< loesche Inputdatei
@@ -214,7 +216,7 @@ private:
     void clearMessage();
     void createActions();
     void createMenus();
-    void createStatusBar();
+    void createStatusBar( const bool showProgressBar );
     void endTool( const int err, const int stopProgress, int &ActionNumber, const QString &FilenameFormat, const int Extension, QStringList &FilenameList, const QString &doneMessage = "Done", const QString &canceledMessage = "Converter was canceled", const bool clearList = false, const bool incActionNumber = true );
     void initFileProgress( const int NumOfFiles, const QString &FilenameIn, const QString &MessageText );
     void initProgress( const int NumOfFiles, const QString &Filename, const QString &MessageText, const int totalNumberOfSteps );
--- trunk/Sources/ApplicationInit.cpp
+++ trunk/Sources/ApplicationInit.cpp
@@ -36,6 +36,20 @@ MainWindow::MainWindow( QWidget *parent ) : QMainWindow( parent )
 
 // **********************************************************************************************
 
+    #if defined(Q_OS_LINUX)
+        gb_showProgressBar = true;
+    #endif
+
+    #if defined(Q_OS_MAC)
+        gb_showProgressBar = false;
+    #endif
+
+    #if defined(Q_OS_WIN)
+        gb_showProgressBar = true;
+    #endif
+
+// **********************************************************************************************
+
     Model       = new QStringListModel( this );
     ListView    = new QListView;
 
@@ -59,7 +73,7 @@ MainWindow::MainWindow( QWidget *parent ) : QMainWindow( parent )
 
     createActions();
     createMenus();
-    createStatusBar();
+    createStatusBar( gb_showProgressBar );
 
 // **********************************************************************************************
 
