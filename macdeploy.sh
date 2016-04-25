5eaebe168d3ea850e828527618baa6377c361e80
Rainer Sieger <rsieger@pangaea.de>
2016-04-25 08:11:12 +0200

    macdeploy -> move application to Applications folder

--- macdeploy.sh
+++ macdeploy.sh
@@ -49,7 +49,7 @@ cd '/Volumes/LatLong Converter'
 rm -rf .fseventsd
 mkdir .fseventsd
 touch .fseventsd/no_log
-cd ~/Development/Distribution
+cd ~/Development/Distribution/LatLongConverter
 
 echo - verify package
 
@@ -57,3 +57,11 @@ codesign -d '/Volumes/LatLong Converter/LatLongConverter.app'
 
 echo
 hdiutil detach '/Volumes/LatLong Converter'
+
+echo - move application
+
+rm -R /Applications/LatLongConverter.app
+cp -R LatLongConverter.app /Applications
+cd ~/Development/Distribution
+
+echo - finished
