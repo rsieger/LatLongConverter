ce71a0ad7e8ef02b6669f33d24b884601fc357b7
Rainer Sieger <github@siegersoft.de>
2016-11-10 22:11:57 +0100

    macdeploy

--- macdeploy.sh
+++ macdeploy.sh
@@ -7,7 +7,7 @@ echo - macdeployqt
 cd ~/Development/GitHub/LatLongConverter
 
 rm -R '../../Distribution/LatLongConverter/LatLongConverter.app'
-cp -R './build-LatLongConverter-Desktop_Qt_5_7_0_clang_64bit2-Release/LatLongConverter.app' '../../Distribution/LatLongConverter/LatLongConverter.app'
+cp -R './build-LatLongConverter-Desktop_Qt_5_7_0_clang_64bit-Release/LatLongConverter.app' '../../Distribution/LatLongConverter/LatLongConverter.app'
 cp './trunk/Resources/Info.plist' '../../Distribution/LatLongConverter/LatLongConverter.app/Contents/Info.plist'
 
 /Developer/Qt/5.7/clang_64/bin/macdeployqt '../../Distribution/LatLongConverter/LatLongConverter.app'
